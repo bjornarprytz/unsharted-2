10ec0f33233efc908d6848eda112e13eb20fa8f1
Brede Eder Murberg <brede@murberg.no>
2024-01-27 19:21:56 +0100

    added assets and made small changes to poop

--- src/object/the_poop.gd
+++ src/object/the_poop.gd
@@ -3,22 +3,37 @@ extends RigidBody2D
 @onready var detection_area : Area2D = $DetectionArea
 @onready var target_scale : Vector2 = scale
 @onready var chunk_spawnr : PackedScene = preload("res://object/chunk.tscn")
-const force : float = 300.0
+const force : float = 1000.0
 
 var size: int = 0
 
-func _input(event: InputEvent) -> void:
-	if (event is InputEventKey and event.keycode == KEY_SPACE and event.is_pressed()):
-		apply_impulse(Vector2.DOWN * force)
-	
-	if (event is InputEventKey and event.keycode == KEY_A and event.is_pressed()):
-		apply_impulse(Vector2.LEFT * force)
-	if (event is InputEventKey and event.keycode == KEY_D and event.is_pressed()):
-		apply_impulse(Vector2.RIGHT * force)
-
-func _physics_process(delta: float) -> void:
+func _process(delta):
 	if !$AnimatedSprite2D.is_playing():
 		$AnimatedSprite2D.play("Idle")
+	
+	if Input.is_action_pressed("left"):
+		apply_force(Vector2.LEFT * force)
+	if Input.is_action_pressed("right"):
+		apply_force(Vector2.RIGHT * force)
+	if Input.is_action_pressed("down"):
+		#if Input.is_action_just_pressed("down"):
+			#$AnimatedSprite2D.play("Windup")
+		#$AnimatedSprite2D.play("Fall")
+		apply_force(Vector2.DOWN * force)
+	
+
+#func _input(event: InputEvent) -> void:
+	#if (event is InputEventKey and event.keycode == KEY_SPACE and event.is_pressed()):
+		#apply_impulse(Vector2.DOWN * force)
+	#
+	#if (event is InputEventKey and event.keycode == KEY_A and event.is_pressed()):
+		#apply_impulse(Vector2.LEFT * force)
+	#if (event is InputEventKey and event.keycode == KEY_D and event.is_pressed()):
+		#apply_impulse(Vector2.RIGHT * force)
+
+func _physics_process(delta: float) -> void:
+	print(linear_velocity)
+	print(angular_velocity)
 
 func _on_detection_area_area_entered(area: Area2D) -> void:
 	var thing = area.owner
@@ -35,10 +50,11 @@ func _on_detection_area_area_entered(area: Area2D) -> void:
 func _increase_size():
 	size += 1
 	target_scale += Vector2.ONE * 0.05
-	print(target_scale)
+	#print(target_scale)
 	scale += (Vector2.ONE * .1)
 	
 	if (size % 5 == 0):
 		add_chunk()
 func add_chunk():
 	var chunk = chunk_spawnr.instantiate()
+	
