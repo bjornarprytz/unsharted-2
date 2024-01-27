f95f53c0bc93d135d70fbbbfb8daf93571697474
Brede Eder Murberg <brede@murberg.no>
2024-01-27 23:32:57 +0100

    Updated main menu

--- src/main_menu.gd
+++ src/main_menu.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+@onready var chunk: Sprite2D = $UI/ChunkSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _process(delta):
+	chunk.rotation_degrees -= 300 * delta 
 
 func _input(event):
 	if (event is InputEventKey and event.keycode == KEY_SPACE and event.is_pressed()):
