fb90bcdae99f2d9b22e27c91d3c204dabd68efab
Bjørnar Prytz <bjornarprytz@gmail.com>
2024-01-28 15:22:51 +0100

    music looping

--- src/autoload/global.gd
+++ src/autoload/global.gd
@@ -12,6 +12,8 @@ signal Fart(magnitude: float)
 signal PoopConsumed
 signal ScoreUpdated(score: int)
 
+@onready var audio :AudioStreamPlayer = $AudioStreamPlayer
+
 var gas_volume : float = 0.0
 
 var score : int:
@@ -23,6 +25,7 @@ var score : int:
 
 func _ready() -> void:
 	Global.PoopConsumed.connect(increment_score.bind(1))
+	audio.finished.connect(audio.play)
 
 func increment_score(value: int):
 	score += value
